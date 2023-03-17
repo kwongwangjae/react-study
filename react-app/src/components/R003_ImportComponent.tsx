91dacc349aca006601aed3f157daec49c8ee41f8
권광재 <kgj9906@naver.com>
2023-03-17 22:28:59 +0900

    feat:3

---
+++ react-app/src/components/R003_ImportComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const R003_ImportComponent = () => {
+  return (
+    <>
+      <h2>[THIS IS IMPORTED COMPONENT]</h2>
+    </>
+  );
+};
+
+export default R003_ImportComponent;
