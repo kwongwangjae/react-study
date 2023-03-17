e56e1bb6323e58b5c6e257ae53d3c36679a183e2
권광재 <kgj9906@naver.com>
2023-03-17 22:43:41 +0900

    fix:3

--- react-app/src/components/R003_ImportComponent.tsx
+++ react-app/src/components/R003_ImportComponent.tsx
@@ -1,11 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 
-const R003_ImportComponent = () => {
-  return (
-    <>
-      <h2>[THIS IS IMPORTED COMPONENT]</h2>
-    </>
-  );
-};
+class R003_ImportComponent extends Component {
+  render() {
+    return <h2>[THIS IS IMPORTED COMPONENT]</h2>;
+  }
+}
 
 export default R003_ImportComponent;
---
+++ react-app/src/components/R004_LifecycleEx.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const R004_LifecycleEx = () => {
+  console.log("3. rendercall");
+  return (
+    <>
+      <h2>[THIS IS RENDER COMPONENT]</h2>
+    </>
+  );
+};
+
+export default R004_LifecycleEx;
