e62a22bec5653242907c4fe0c4fffda1675cb7f6
권광재 <kgj9906@naver.com>
2023-03-17 21:51:40 +0900

    feat:1

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div>한국</div>;
+  return (
+    <div>
+      <h1>Start react 200!</h1>
+      <p>HTML 적용하기</p>
+    </div>
+  );
 }
 
 export default App;
