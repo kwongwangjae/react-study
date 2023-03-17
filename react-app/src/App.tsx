4ea18da7f21225e747d28384fc8716c99ea92333
권광재 <kgj9906@naver.com>
2023-03-17 22:09:32 +0900

    feat:2

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Start react 200!</h1>
+    <Div>
+      <H1>Start react 200!</H1>
       <p>HTML 적용하기</p>
-    </div>
+    </Div>
   );
 }
 
 export default App;
+
+const Div = styled.div`
+  background-color: rgb(162, 127, 243);
+  color: rgb(255, 255, 255);
+  padding: 40px;
+  font-family: 고딕;
+  text-align: center;
+`;
+const H1 = styled.h1`
+  color: white;
+  background-color: #2efe2e;
+  padding: 10px;
+  font-family: 궁서;
+`;
